d3269c97c9484387cd905672c6e8c4bdfdd1806b
Benoit <Benoit.poulet@edu.ece.fr>
2026-03-27 09:01:24 +0100

    avancée sur heston et mcmc

---
+++ A.4 Modélisation de volatilté stochastique (Heston/SABR) avec MCMC/src/models/sabr_model.py
@@ -0,0 +1,13 @@
+"""
+Modèle probabiliste SABR implémenté avec NumPyro.
+
+À implémenter ultérieurement.
+"""
+
+class SABRModel:
+    """
+    Modèle probabiliste SABR pour l'inférence bayésienne avec NumPyro.
+    
+    À implémenter.
+    """
+    pass
---
+++ A.4 Modélisation de volatilté stochastique (Heston/SABR) avec MCMC/src/simulation/sabr_sim.py
@@ -0,0 +1,13 @@
+"""
+Simulation du modèle SABR (Stochastic Alpha Beta Rho).
+
+À implémenter ultérieurement.
+"""
+
+class SABRSimulator:
+    """
+    Simulateur pour le modèle SABR.
+    
+    À implémenter.
+    """
+    pass
